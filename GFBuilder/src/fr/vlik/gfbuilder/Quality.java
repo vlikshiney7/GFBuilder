e11308b01e27ff14e02eb9bcefebd1fad1b2f2a2
vlikshiney7 <vlikshiney7@laposte.net>
2019-09-04 11:35:57 +0200

    Version 0.2.5
    - Add grandfantasia package
    - All resources was moved in
    - Add Mount

---
+++ GFBuilder/src/fr/vlik/gfbuilder/Quality.java
@@ -0,0 +1,15 @@
+package fr.vlik.gfbuilder;
+
+public enum Quality {
+	GREY(0), WHITE(1),
+	GREEN(2), BLUE(3),
+	ORANGE(4), GOLD(5),
+	PURPLE(6), RED(7),
+	P8TITLE(8);
+	
+	public final int index;
+	 
+    private Quality(int index) {
+        this.index = index;
+    }
+}
